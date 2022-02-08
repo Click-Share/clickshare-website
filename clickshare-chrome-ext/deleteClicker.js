cb6996b170ba00f3a687bd1133158b4ad06c0f03
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-02-08 17:09:44 -0500

    removed chrome extension folder

--- clickshare-chrome-ext/deleteClicker.js
+++
@@ -1,8 +0,0 @@
-const removeCursor = () => {
-    var element = document.getElementById('clickshare-cursor');
-    if (element) {
-      element.parentElement.removeChild(element);
-    }
-}
-  
-setInterval(removeCursor, 1500);
